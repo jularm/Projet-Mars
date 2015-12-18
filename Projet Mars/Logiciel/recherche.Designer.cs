9cd306843c2544f937ea9f720195a8b57338a2a6
jularm <julianarmand@gmail.com>
2015-12-18 17:34:34 +0100

    changement dans la recherche

--- Projet Mars/Logiciel/recherche.Designer.cs
+++ Projet Mars/Logiciel/recherche.Designer.cs
@@ -102,7 +102,7 @@
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(119, 41);
             this.button2.TabIndex = 49;
-            this.button2.Text = "Supprimer les résultats";
+            this.button2.Text = "Nouvelle recherche";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
